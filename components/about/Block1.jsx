f00c9802d4da6bb1fcb5873d7c92e8497fe86552
rakibmorsalin1992 <rakib.islam0000@gmail.com>
2024-12-04 16:50:46 +0600

    about page design completed

--- components/about/Block1.jsx
+++ components/about/Block1.jsx
@@ -113,7 +113,8 @@ const Block1 = () => {
           className="rounded-4 w-100"
         />
       </div> */}
-      <div className="home2-about-section pt-120 mb-120">
+      {/* new design as tripex */}
+      {/* <div className="home2-about-section pt-120 mb-120">
         <div className="container">
           <div className="row mb-90">
             <div className="col-lg-6">
@@ -157,6 +158,122 @@ const Block1 = () => {
                   className="vector"
                 />
               </div>
+            </div>
+          </div>
+        </div>
+      </div> */}
+      <div className="about-section-design">
+        <div className="simple-image">
+          <Image
+            src="/img/about/svgviewer-output.svg"
+            alt="About Image"
+            width={273.95}
+            height={300.42}
+          ></Image>
+        </div>
+        <div className="simple-image-two">
+          <Image
+            src="/img/about/svgviewer-output.svg"
+            alt="About Image"
+            width={203.73}
+            height={223.42}
+          ></Image>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className=" text-center content">
+                <div className="top-content">
+                  <h2>Our story</h2>
+                  <p>
+                    Once upon a time, nestled in the heart of Rome, there was a
+                    team of travel enthusiasts who shared one common passion: to
+                    turn ordinary vacations into extraordinary adventures. That
+                    team was us – Dream Tourism!
+                  </p>
+                </div>
+                <div className="mt-40 mb-40 content-image">
+                  <Image
+                    src="/img/about/image.png"
+                    alt="About Image"
+                    width={844}
+                    height={444}
+                  ></Image>
+                </div>
+                <div className="main-content ">
+                  <p className="mb-10">
+                    Our story began with a simple idea: to create unforgettable
+                    experiences for travelers like you. Armed with a deep love
+                    for exploration and a knack for planning, we set out to
+                    redefine what it means to travel.
+                  </p>
+                  <p className="mb-10">
+                    Picture this: skipping the long lines and diving straight
+                    into the wonders of the Colosseum. Feeling the awe-inspiring
+                    presence of history at the Vatican. Exploring the hidden
+                    gems of Capri and uncovering the secrets of Pompeii. These
+                    were the dreams we wanted to turn into reality for every
+                    traveler who crossed our path.
+                  </p>
+                  <p className="mb-10">
+                    From the moment we opened our doors in 2016, our journey has
+                    been nothing short of magical. We've whisked adventurers
+                    away on captivating tours across Europe – from the charming
+                    streets of Germany to the sun-kissed shores of Spain.
+                  </p>
+                  <p className="mb-10">
+                    But our success isn't just measured by the miles we've
+                    traveled or the landmarks we've seen. It's measured by the
+                    smiles on the faces of our customers, by the memories we've
+                    helped create, and by the stories that live on long after
+                    the journey ends.
+                  </p>
+                  <p className="mb-10">
+                    With over 10,000 travelers served and countless rave reviews
+                    on platforms like Viator and TripAdvisor, we've earned our
+                    stripes as one of the top travel companies in the business.
+                    But for us, it's not about accolades or awards – it's about
+                    sharing our passion for travel with the world.
+                  </p>
+                  <p className="mb-10">
+                    So, whether you're a seasoned explorer or a first-time
+                    adventurer, we invite you to join us on a journey like no
+                    other. Let's turn your travel dreams into unforgettable
+                    stories with dreamtourism.co.uk. After all, the best stories
+                    are the ones we live.
+                  </p>
+                  <p>
+                    Need your feedback and input so I can make it better or just
+                    perfect.
+                  </p>
+                </div>
+                <div className="footer-content">
+                  {/* <div className="footer-content-image">
+                    <Image
+                      src="/img/about/svgviewer-output.svg"
+                      alt="About Image"
+                      width={185.51}
+                      height={203.44}
+                    ></Image>
+                  </div> */}
+                  <div className="content-details">
+                    <div className="">
+                      <p>Company registered name :</p>
+                      <p>Registration Number : </p>
+                      <p>PIVA: </p>
+                      <p>Contact Number :</p>
+                    </div>
+                    <div className="">
+                      <p>Dream Tourism DMC</p>
+                      <p>Registration Number</p>
+                      <p>N/B</p>
+                      <p>+3906 4525 9865</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div></div>
             </div>
           </div>
         </div>
