58021fd0eca162f8c8af85dadd6f436f759dca42
rakibmorsalin1992 <rakib.islam0000@gmail.com>
2024-12-08 18:21:24 +0600

    abount section design completed without image

--- components/about/Block1.jsx
+++ components/about/Block1.jsx
@@ -185,12 +185,19 @@ const Block1 = () => {
             <div className="col-12">
               <div className=" text-center content">
                 <div className="top-content">
-                  <h2>Our story</h2>
+                  <h2 className="mb-10">
+                    About Dream Tourism SRLS: Your Trusted Travel Partner in
+                    Italy
+                  </h2>
                   <p>
-                    Once upon a time, nestled in the heart of Rome, there was a
-                    team of travel enthusiasts who shared one common passion: to
-                    turn ordinary vacations into extraordinary adventures. That
-                    team was us – Dream Tourism!
+                    At Dream Tourism, our mission is simple yet powerful:{" "}
+                    <b>to make travel easy, memorable, and truly worthwhile.</b>{" "}
+                    Founded with a passion for exploration and a deep
+                    understanding of what travelers seek, we are dedicated to
+                    providing exceptional experiences that go beyond
+                    expectations. As avid travelers ourselves, we know how
+                    important each journey is, and we've made it our mission to
+                    offer the best service possible.
                   </p>
                 </div>
                 <div className="mt-40 mb-40 content-image">
@@ -201,33 +208,45 @@ const Block1 = () => {
                     height={444}
                   ></Image>
                 </div>
-                <div className="main-content ">
+                <div className="main-content text-left">
+                  <h5 className="text-left mb-10">What Sets Us Apart?</h5>
                   <p className="mb-10">
-                    Our story began with a simple idea: to create unforgettable
-                    experiences for travelers like you. Armed with a deep love
-                    for exploration and a knack for planning, we set out to
-                    redefine what it means to travel.
+                    Dream Tourism is proud to be one of the{" "}
+                    <b>largest suppliers of Colosseum and Vatican tickets,</b>{" "}
+                    offering seamless access to Rome's most iconic sites. Our
+                    exclusive tours in <b>Rome</b> and <b>Capri Island</b> stand
+                    out due to our local expertise and commitment to
+                    personalized service. Whether you’re skipping the ticket
+                    line at the Colosseum or exploring the beauty of Capri, we
+                    ensure a smooth, enriching experience.
                   </p>
+                  <h5 className="mb-10">Our Popular Tours:</h5>
+
                   <p className="mb-10">
-                    Picture this: skipping the long lines and diving straight
-                    into the wonders of the Colosseum. Feeling the awe-inspiring
-                    presence of history at the Vatican. Exploring the hidden
-                    gems of Capri and uncovering the secrets of Pompeii. These
-                    were the dreams we wanted to turn into reality for every
-                    traveler who crossed our path.
+                    Our most sought-after offering is the Skip-the-Line
+                    Colosseum Tour, designed to save you time while providing
+                    in-depth insights into ancient history. We specialize in
+                    Rome and its treasures, ensuring you experience the heart of
+                    Italy with authenticity and ease.
                   </p>
+                  <h5 className="mb-10">Our Values:</h5>
+
                   <p className="mb-10">
-                    From the moment we opened our doors in 2016, our journey has
-                    been nothing short of magical. We've whisked adventurers
-                    away on captivating tours across Europe – from the charming
-                    streets of Germany to the sun-kissed shores of Spain.
+                    We believe in being an easy-to-reach, locally registered
+                    company that travelers can trust. Our core values focus on
+                    delivering the best customer service with genuine care,
+                    ensuring that we provide you with the best things to do in
+                    Rome.
                   </p>
+                  <h5 className="mb-10">Meet Our Team:</h5>
+
                   <p className="mb-10">
-                    But our success isn't just measured by the miles we've
-                    traveled or the landmarks we've seen. It's measured by the
-                    smiles on the faces of our customers, by the memories we've
-                    helped create, and by the stories that live on long after
-                    the journey ends.
+                    Dream Tourism is powered by a team of professional customer
+                    support executives, local expert guides, and passionate
+                    hosts. Each member is committed to making your journey
+                    unforgettable. From continuous communication after booking
+                    to having a dedicated host for your Colosseum tour, we
+                    prioritize your comfort and satisfaction.
                   </p>
                   <p className="mb-10">
                     With over 10,000 travelers served and countless rave reviews
@@ -236,19 +255,24 @@ const Block1 = () => {
                     But for us, it's not about accolades or awards – it's about
                     sharing our passion for travel with the world.
                   </p>
+                  <h5 className="mb-10">Trusted Partnerships:</h5>
+
                   <p className="mb-10">
-                    So, whether you're a seasoned explorer or a first-time
-                    adventurer, we invite you to join us on a journey like no
-                    other. Let's turn your travel dreams into unforgettable
-                    stories with dreamtourism.co.uk. After all, the best stories
-                    are the ones we live.
+                    We are proud partners of leading platforms like TripAdvisor,
+                    Viator, GetYourGuide, Klook, and many more. These
+                    partnerships reflect our commitment to quality and
+                    reliability in the global travel community.
                   </p>
+                  <h5 className="mb-10">A Personal Promise:</h5>
+
                   <p>
-                    Need your feedback and input so I can make it better or just
-                    perfect.
+                    "If you book with us, you’re not just purchasing a
+                    tour—you’re securing the best travel experience in Rome. We
+                    take care of the details, so you can focus on making
+                    memories."
                   </p>
                 </div>
-                <div className="footer-content mt-40">
+                <div className="footer-content mt-40 mb-40">
                   {/* <div className="footer-content-image">
                     <Image
                       src="/img/about/svgviewer-output.svg"
@@ -265,10 +289,10 @@ const Block1 = () => {
                       <p>Contact Number :</p>
                     </div>
                     <div className="">
-                      <p>Dream Tourism DMC</p>
-                      <p>Registration Number</p>
-                      <p>N/B</p>
-                      <p>+3906 4525 9865</p>
+                      <p>Dream Tourism SRLS</p>
+                      <p>RM - 1575592</p>
+                      <p>15216381002</p>
+                      <p>+39 388 774 8015</p>
                     </div>
                   </div>
                 </div>
