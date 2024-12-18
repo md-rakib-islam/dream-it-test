b0a2a64535cec63cbfe4656321935a25518871c2
rakibmorsalin1992 <rakib.islam0000@gmail.com>
2024-12-18 09:51:39 +0600

    push

--- constant/constants.js
+++ constant/constants.js
@@ -1,6 +1,7 @@
 //base url
 export const BASE_URL = "http://192.168.68.125:8001";
 // export const BASE_URL = "https://api.dreamtourism.it";
+// export const BASE_URL = "https://api.dreamtourism.it";
 
 // slidersettings
 export const CREATE_SLIDERSETTING = `${BASE_URL}/homepage_slider/api/v1/homepage_slider/create/`;
