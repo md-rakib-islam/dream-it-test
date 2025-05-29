8f207332a10aefbe81cd74a2708eebace808e5a3
rakibmorsalin1992 <rakib.islam0000@gmail.com>
2025-05-29 17:59:48 +0600

    fix: Update Google Tag Manager ID in layout component for tracking purposes

--- app/layout.jsx
+++ app/layout.jsx
@@ -163,7 +163,7 @@ export default async function RootLayout({ children }) {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-THMNLX34');
+            })(window,document,'script','dataLayer','AW-16851202879');
           `,
           }}
         />
@@ -199,7 +199,7 @@ export default async function RootLayout({ children }) {
             {/* Google Tag Manager (noscript) */}
             <noscript
               dangerouslySetInnerHTML={{
-                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-THMNLX34"
+                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=AW-16851202879"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
               }}
             />
