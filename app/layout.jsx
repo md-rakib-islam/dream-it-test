f2b4faaafee0dcfc528f8a17e2377e27d8c0b49d
rakibmorsalin1992 <rakib.islam0000@gmail.com>
2025-06-19 16:56:44 +0600

    feat: Add second Facebook Pixel for enhanced tracking

--- app/layout.jsx
+++ app/layout.jsx
@@ -179,17 +179,19 @@ export default async function RootLayout({ children }) {
         {/* Facebook Meta Pixel */}
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '551593183083152');
-            fbq('track', 'PageView');
-          `}
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+
+          fbq('init', '551593183083152');
+          fbq('init', '1943853973058055');
+          fbq('track', 'PageView');
+        `}
         </Script>
 
         {/* Structured Data */}
@@ -205,7 +207,14 @@ export default async function RootLayout({ children }) {
             width="1"
             style={{ display: "none" }}
             src="https://www.facebook.com/tr?id=551593183083152&ev=PageView&noscript=1"
-            alt="fb-pixel"
+            alt="fb-pixel-1"
+          />
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1943853973058055&ev=PageView&noscript=1"
+            alt="fb-pixel-2"
           />
         </noscript>
 
